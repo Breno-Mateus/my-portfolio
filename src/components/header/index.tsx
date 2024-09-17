504fe53d9185aa79995cdd44062e9a85830b7c36
Breno-Mateus <brenomateus36@gmail.com>
2024-09-17 18:18:50 -0300

    add button link

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -11,7 +11,7 @@ const Header = () => {
             <Link to="SectionAbout" smooth={true} duration={500}> <Text>Sobre</Text> </Link>
             <Link to="SectionSkills" smooth={true} duration={500}> <Text>Habilidades</Text> </Link>
             <Link to="SectionProjects" smooth={true} duration={500}> <Text>Projetos</Text> </Link>
-            <Button href="malito:brenomateus36@gmail.com?subject=Contato pelo Portfólio&body=Olá, gostaria de falar sobre..." text="Contato"/>
+            <Button href="https://www.linkedin.com/in/breno-mateus-8b9a99211/" text="Contato"/>
         </Cabecalho>
     )
 }
