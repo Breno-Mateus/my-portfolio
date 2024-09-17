7988b39a405c928ae338e50ee2e570a9573a2797
Breno-Mateus <brenomateus36@gmail.com>
2024-09-17 12:30:13 -0300

    add skill

--- src/components/sectionskills/index.tsx
+++ src/components/sectionskills/index.tsx
@@ -10,6 +10,7 @@ import Git from "../../assets/Skills_Image/git.png"
 import Typescript from "../../assets/Skills_Image/Typescript.png"
 import Style from "../../assets/Skills_Image/stylecomponents.png"
 import Node from "../../assets/Skills_Image/nodejs.png"
+import linux from "../../assets/Skills_Image/linux.png"
 
 const SectionSkills = () => {
     return (
@@ -25,6 +26,7 @@ const SectionSkills = () => {
                 <SkillsBox image={Git} text="Git"/>
                 <SkillsBox image={Typescript} text="TypeScript"/>
                 <SkillsBox image={Style} text="Styled-Components"/>
+                <SkillsBox image={linux} text="Linux"/>
             </SecaoSkills>
         </SecaoPrincipal>
     )
