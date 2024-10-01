a4d04d43273d45a8bd758b7956436a510352f8de
Breno-Mateus <brenomateus36@gmail.com>
2024-10-01 07:11:53 -0300

    feat: add news skills

--- src/sections/sectionskills/index.tsx
+++ src/sections/sectionskills/index.tsx
@@ -11,6 +11,13 @@ import Typescript from "../../assets/Skills_Image/Typescript.png"
 import Style from "../../assets/Skills_Image/stylecomponents.svg"
 import Node from "../../assets/Skills_Image/nodejs.png"
 import linux from "../../assets/Skills_Image/linux.png"
+import figma from "../../assets/Skills_Image/figma.png"
+import postman from "../../assets/Skills_Image/postman.png"
+import jest from "../../assets/Skills_Image/jest.svg"
+import testing from "../../assets/Skills_Image/Testing-Library.svg"
+import tailwind from "../../assets/Skills_Image/Tailwind CSS.png"
+import Vercel from "../../assets/Skills_Image/Vercel.png"
+
 
 const SectionSkills = () => {
     return (
@@ -25,8 +32,14 @@ const SectionSkills = () => {
                 <SkillsBox image={ImageReact} text="React"/>
                 <SkillsBox image={Git} text="Git"/>
                 <SkillsBox image={Typescript} text="TypeScript"/>
+                <SkillsBox image={tailwind} text="Tailwind CSS"/>
+                <SkillsBox image={jest} text="Jest"/>
+                <SkillsBox image={testing} text="Testing Library"/>
                 <SkillsBox image={Style} text="Styled-Components"/>
                 <SkillsBox image={linux} text="Linux"/>
+                <SkillsBox image={figma} text="Figma"/>
+                <SkillsBox image={Vercel} text="Vercel"/>
+                <SkillsBox image={postman} text="Postman"/>
             </SkillSection>
         </SectionStyle>
     )
