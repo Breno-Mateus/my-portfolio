987acb933e708a6fe7b4d102ff9ed6edba844ce4
Breno-Mateus <brenomateus36@gmail.com>
2024-10-29 19:21:59 -0300

    feat: add news skills

--- src/sections/sectionskills/index.tsx
+++ src/sections/sectionskills/index.tsx
@@ -11,6 +11,7 @@ import Typescript from "../../assets/Skills_Image/Typescript.png"
 import Style from "../../assets/Skills_Image/stylecomponents.svg"
 import Node from "../../assets/Skills_Image/nodejs.png"
 import linux from "../../assets/Skills_Image/linux.png"
+import tailwind from "../../assets/Skills_Image/Tailwind CSS.png"
 
 const SectionSkills = () => {
     return (
@@ -26,6 +27,7 @@ const SectionSkills = () => {
                 <SkillsBox image={Git} text="Git"/>
                 <SkillsBox image={Typescript} text="TypeScript"/>
                 <SkillsBox image={Style} text="Styled-Components"/>
+                <SkillsBox image={tailwind} text="Tailwind CSS"/>
                 <SkillsBox image={linux} text="Linux"/>
             </SkillSection>
         </SectionStyle>
